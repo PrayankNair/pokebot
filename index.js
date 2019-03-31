fdfc9ba57412237f218c8fb313ed9a2a307623fc
PrayankNair <49112765+PrayankNair@users.noreply.github.com>
2019-03-31 11:42:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const port = process.env.PORT || 443,
     host = '0.0.0.0', // probably this change is not required
-    externalUrl = process.env.HOSTURL,
-    token = process.env.TOKEN || 'YOUR TELEGRAM TOKEN',
+    externalUrl = "https://techinfinite.herokuapp.com/",
+    token = process.env.TOKEN || '840101094:AAGBLogpFKyJQrRiEwp1Gr8H7BD7P11ieLw',
     bot = new TelegramBot(token, {
         webHook: {
             port: port,
@@ -11,3 +11,7 @@ const port = process.env.PORT || 443,
         }
     });
 bot.setWebHook(externalUrl + ':443/bot' + token);
+bot.on('message',(msg) =>{
+    const chatId = msg.chat.chatId
+    bot.sendMessage(chatId,'Received Your Message')
+})
